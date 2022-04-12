6147cb3182eb4880985539621405fd4106c10bc4
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-04-12 11:49:46 +0100

    Modifty the argument recieve and move the reading of the file in the main function

--- function/validate.go
+++ function/validate.go
@@ -1,8 +1,6 @@
 package function
 
 import (
-	"io/ioutil"
-	"os"
 	"strconv"
 	"strings"
 )
@@ -12,21 +10,19 @@ import (
 //the file has start and end points.
 //It will return true and the content of the file in an array if the file is formatted correctly and
 // return false and an error message for invalid file.
-func ValidateFile(file string) (bool, []string) {
-	s, _ := os.Open(file)                    // open the file
-	f, _ := ioutil.ReadAll(s)                // read the file
-	splitF := strings.Split(string(f), "\n") // split the data with newline and assign it to splitF
-	if len(splitF) == 1 {                    //check if the length of the file is 1 and return error
+func ValidateFile(fileByte []byte) (bool, []string) {
+	splitF := strings.Split(string(fileByte), "\n") // split the data with newline and assign it to splitF
+	if len(splitF) == 1 {                           //check if the length of the file is 1 and return error
 		return false, []string{"ERROR: invalid data format, empty file"}
 	}
 	num, err := strconv.Atoi(splitF[0])
 	if num == 0 || splitF[0] == "" || err != nil { // checks if their is a valid number of ants
 		return false, []string{"ERROR: invalid data format, invalid number of Ants"}
 	}
-	if !(strings.Contains(string(f), "##start")) { // checks if their is a start room
+	if !(strings.Contains(string(fileByte), "##start")) { // checks if their is a start room
 		return false, []string{"ERROR: invalid data format, no start room found"}
 	}
-	if !(strings.Contains(string(f), "##end")) { // checks if their is a end room
+	if !(strings.Contains(string(fileByte), "##end")) { // checks if their is a end room
 		return false, []string{"ERROR: invalid data format, no end room found"}
 	}
 	return true, splitF // return true and the whole array of file
